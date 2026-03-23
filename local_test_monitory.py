127dc80063b05b80fe8d969fb1c4ae28e3e8944a
wrongu <opensource@bonsai-neuro-ai.com>
2026-03-23 07:52:03 -0400

    locally run/test multiple cameras

--- local_test_monitory.py
+++ local_test_monitory.py
@@ -1,4 +1,6 @@
+import logging
 from pathlib import Path
+from pprint import pprint
 
 import cv2 as cv
 
@@ -7,23 +9,24 @@ from camera_monitor import CameraMonitor
 from cameras import ONVIFCameraWrapper, LoggedImagePseudoCamera
 
 
-def run(monitor: CameraMonitor, poll_interval: float):
+def run(*monitors: CameraMonitor, poll_interval: float):
     while True:
-        monitor.poll()
-        _, frame = monitor.camera.get_last_frame()
-        for b in monitor.last_blobs:
-            x, y, w, h = b.bbox
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv.putText(
-                frame,
-                str(b.class_id),
-                (x - 10, y - 10),
-                cv.FONT_HERSHEY_SIMPLEX,
-                0.9,
-                (36, 255, 12),
-                2,
-            )
-        cv.imshow("Frame", frame)
+        for monitor in monitors:
+            monitor.poll()
+            _, frame = monitor.camera.get_last_frame()
+            for b in monitor.last_blobs:
+                x, y, w, h = b.bbox
+                cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv.putText(
+                    frame,
+                    str(b.class_id),
+                    (x - 10, y - 10),
+                    cv.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (36, 255, 12),
+                    2,
+                )
+            cv.imshow("Frame" + str(monitor), frame)
         k = cv.waitKey(int(poll_interval * 1000)) & 0xFF
         if k == ord("q"):
             break
@@ -44,43 +47,49 @@ if __name__ == "__main__":
         secrets = yaml.safe_load(f)
 
     with open("local_config.yaml", "r") as f:
-        config = yaml.safe_load(f)
+        app_config = yaml.safe_load(f)
+
+    pprint(app_config)
+
+    monitors = []
+    for config in app_config["cameras"]:
+        mon = CameraMonitor(
+            camera=ONVIFCameraWrapper(**secrets[config["name"]]),
+            name=config["name"],
+            brightness_threshold=config["brightness_threshold"],
+            history_seconds=config["history_seconds"],
+            bg_model=TimestampAwareBackgroundSubtractor(
+                history_seconds=config["history_seconds"],
+                var_threshold=config["var_threshold"],
+                detect_shadows=config["detect_shadows"],
+                area_threshold=config["area_threshold"],
+                shadow_correlation_threshold=config["shadow_correlation_threshold"],
+                morph_radius=config["morph_radius"],
+                morph_thresh=config["morph_thresh"],
+                morph_iters=config["morph_iters"],
+                default_fps=1 / app_config["poll_frequency"],
+                region_of_interest=cv.imread(
+                    config["region_of_interest"], cv.IMREAD_GRAYSCALE
+                ),
+                night_mode_kwargs={
+                    k[6:]: v for k, v in config.items() if k.startswith("night_")
+                },
+            ),
+            save_blobs=config.get("save_blobs", True),
+            model_file=config.get("model_file", None),
+            output_dir=config["output_dir"],
+            log=lambda msg, level="INFO": logging.log(level=logging.INFO, msg=f"[{level}]: {msg}"),
+            on_state_transition=print,
+            on_detection=print,
+        )
+        monitors.append(mon)
 
     if args.live:
-        camera = ONVIFCameraWrapper(
-            host=secrets["host"],
-            port=secrets["port"],
-            username=secrets["username"],
-            password=secrets["password"],
-            resolution=tuple(secrets["resolution"]),
-        )
+        pass
     elif args.log:
-        camera = LoggedImagePseudoCamera(args.data_dir)
+        for monitor in monitors:
+            monitor.camera = LoggedImagePseudoCamera(monitor.output_dir)
     else:
         raise ValueError("Must specify either --live or --log")
 
-    bg_model = TimestampAwareBackgroundSubtractor(
-        history_seconds=config["history_seconds"],
-        var_threshold=config["var_threshold"],
-        detect_shadows=config["detect_shadows"],
-        area_threshold=config["area_threshold"],
-        shadow_correlation_threshold=config["shadow_correlation_threshold"],
-        morph_radius=config["morph_radius"],
-        morph_thresh=config["morph_thresh"],
-        morph_iters=config["morph_iters"],
-        default_fps=config["default_fps"],
-        night_mode_kwargs={
-            k[6:]: v for k, v in config.items() if k.startswith("night_")
-        },
-        region_of_interest=cv.imread(config["region_of_interest"], cv.IMREAD_GRAYSCALE),
-        debug_dir=Path("debug"),
-    )
-    monitor = CameraMonitor(
-        camera=camera,
-        name="DebugMonitor",
-        brightness_threshold=config["brightness_threshold"],
-        history_seconds=config["history_seconds"],
-        bg_model=bg_model,
-        output_dir=args.data_dir if args.live else None,
-    )
-    run(monitor, config["poll_frequency"])
+    run(*monitors, poll_interval=app_config["poll_frequency"])
